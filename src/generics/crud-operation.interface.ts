14a16bf5eca5d3b71f02982798761adfd5c90791
asmaSmida <asmasmida@insat.u-carthage.tn>
2022-05-27 17:30:24 +0100

    initializing project + starting auth module

---
+++ src/generics/crud-operation.interface.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+export interface CrudOperations<T, ID> {
+  create(t: T): Promise<T>;
+  update(id: ID, t: T): Promise<T>;
+  findOne(id: ID): Promise<T>;
+  findAll(): Promise<T[]>;
+  remove(id: ID): Promise<any>;
+}
